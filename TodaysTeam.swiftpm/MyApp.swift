0ce129565913c3ab0585c72613a1c3a390d73a22
MADElinessss <jyseen@naver.com>
2023-03-29 14:25:42 +0900

    [feat] 메인 화면, tab bar, 화면 연결

--- TodaysTeam.swiftpm/MyApp.swift
+++ TodaysTeam.swiftpm/MyApp.swift
@@ -4,7 +4,30 @@ import SwiftUI
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                HomeImageView()
+                    .tabItem {
+                        Label("홈", systemImage: "house")
+                    }
+                HomeView()
+                    .tabItem {
+                        Label("둘러보기", systemImage: "doc.on.doc")
+                    }
+                ShoppingView()
+                    .tabItem {
+                        Label("쇼핑", systemImage: "basket")
+                    }
+                ProfView()
+                    .tabItem {
+                        Label("전문업체찾기", systemImage: "person.2.badge.gearshape.fill")
+                    }
+                ProfileView()
+                    .badge(1)
+                    .tabItem {
+                        Label("마이페이지", systemImage: "person")
+                    }
+            }
+            .font(.footnote)
         }
     }
 }
