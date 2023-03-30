c64a85190fdc5247da1ee784aab19ef85c1842f1
samilulu <samcha@10-141-61-223.0200.pos.kr.iosda.org>
2023-03-30 17:07:12 +0900

    {feat} 쨈 세부정보

---
+++ TodaysTeam.swiftpm/Za/ZamTemplate.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by changgyo seo on 2023/03/27.
+//
+
+import SwiftUI
+
+extension Style {
+    static var zam: [Style] = [
+        .style13(title: "ZAM"),
+        .style4(image: ["zam_4_1", "zam_4_2", "zam_4_3"])
+        .style13(title: "살다살다 이렇게 메모하는사람 처음 봤습니다. 그냥 옆에서 지켜보고 있으면 입이 떠억 벌어진답니다. 턱 빠져요 그냥. 그리고 질문 요정입니다. 디자인 실력은 말하면 입만 아프죠. 리액션도 상당히 훌륭합니다."),
+        .style5(title: "메모로 돌아가는 뇌📝🧠", subTitle: "항상 메모하면서 생각을 해요. 회의 내용도 잘 정리하고, 다음에 해야하는 것은 무엇인지 이정표도 잘 세우고, 회의 내용이 실행 될 수 있게 기여합니다."),
+        .style11(image: "zam_11", title: "WHYWHYWHYWHY\nWHYWHYWHYWHYWHY\nWHYWHYWHYWHYWHY\nWHYWHYWHYWHYWHY"),
+        .style5(title: "(왜왜왜-TVXQ)", subTitle: "질문 요정입니다. 궁금한게 많아요. 왜 라는 질문을 많이 하기 때문에\n프로젝트 할 때도 근본적인 문제인식과 해결책을 잘 매치시킵니다.\n항상 다른 사람이 말하는 것을 경청하고 노트정리를 합니다."),
+        .style8(image: "zam_8", title: "개그코드가 분명해요..", subTitle:"이상한 단어로\n이틀 내내 빵터질 수도 있어요\nex. 여귄(여자펭귄)"),
+        .style14(image: "zam_14_1"),
+        .style5(title: "메인 랩에 쨈을\n오랜 시간 방치해두었다면..", subTitle: "꼭 산책하러 나가주세요..\n나갔다 오면 다시 밝아진 쨈을 볼 수 있습니다."),
+        .style2(image: "zam_2_1", title: "MC1에서\n친밀하고 즐거운 팀워크의 힘을 배웠어요"),
+        .style2(image: "zam_2_2", title: "다음 MC2에서는?\n회고하기\n디자인과 UX를 팀 내에 전파하기\n를 하고싶어요"),
+        .style14(image: "zam_14_2"),
+        .style15(title: "팀원들 한 명 한 명의 의견을 모두 듣고 존중해주는 멋있는 사람입니다 데려가세요"),
+    ]
+}
