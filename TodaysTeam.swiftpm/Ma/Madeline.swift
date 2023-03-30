ebf86884900a7b5c74ee82c24ae3fb623c948cef
MADElinessss <jyseen@naver.com>
2023-03-30 16:31:35 +0900

    [feat] 매들린 세부정보

---
+++ TodaysTeam.swiftpm/Ma/Madeline.swift
@@ -0,0 +1,19 @@
+//
+//  Madeline.swift
+//  
+//
+//  Created by 신정연 on 2023/03/30.
+//
+
+import Foundation
+
+extension Style {
+    static var Madeline: [Style] = [
+        .style1(image: "Mad9", title: "[당일출고]\n해피 쿼카 출신 개발자"),
+        .style9(image: "Mad1", title: "“마들렌 아니고 매들린 입니다”", subTitle: "파트는 테크지만, (아직은) 영어를 더 잘합니다.\n팀 분위기가 좋으면 능률이 오릅니다.\n\n안드로이드하다가 왔습니다.\n이제는 디자인도, 서버도 찍먹 해보고 싶어요"),
+        .style4(image: ["Mad6","Mad3","Mad7"]),
+        .style8(image:"Mad4", title: "사람을 좋아하는 엔프피", subTitle: "웃음이 많아서 팀 분위기를\n밝게 만드는 능력이 있습니다!눈치가 엄청 빨라요.\n\n어디에나 잘어울리는 개발자,\n신제품 특가로 지금 만나보세요!\n→"),
+        .style10(title: "매들린이 행복해보이는 순간은?", subTitle: "은근 개그 욕심이 있어서\n팀원들을 빵터지게 할 때 행복해보입니다.\n회식할때! 소주만 먹는 멋진 상여자 입니다."),
+        .style11(image: "Mad2", title: "매들린 데려가실 팀은 쌀\n국수 한 번으론  안되구요.\n여러 번 사ㅣ 사사주세요.\n쌀국수 쌀국수 쌀국수 쌀")
+    ]
+}
