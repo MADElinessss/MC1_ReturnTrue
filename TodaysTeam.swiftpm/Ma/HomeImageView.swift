1c654cfd777e79b45bf89354e5caa6b02ffeca38
MADElinessss <jyseen@naver.com>
2023-03-31 00:23:41 +0900

    [feat] SplashView 첫화면에 추가

--- TodaysTeam.swiftpm/Ma/HomeImageView.swift
+++ TodaysTeam.swiftpm/Ma/HomeImageView.swift
@@ -8,37 +8,66 @@
 import SwiftUI
 
 struct HomeImageView: View {
-    
+    @State private var showSplash = true
+    @State private var showSplash2 = false
     @State private var searchText = ""
     
     var body: some View {
         NavigationView {
-            VStack{
-                HStack{
-                    SearchBar(text: $searchText)
-                        .padding(.leading, 25)
-                        .padding(.trailing, 10)
-                    Image(systemName: "bookmark")
-                        .font(.system(size: 25, weight: .light))
-                        .imageScale(.medium)
-                        .frame(width: 40, height: 40)
-                    NavigationLink(destination: CartView()){
-                        Image(systemName: "cart")
-                            .foregroundColor(.black)
+            ZStack {
+                VStack{
+                    HStack{
+                        SearchBar(text: $searchText)
+                            .padding(.leading, 25)
+                            .padding(.trailing, 10)
+                        Image(systemName: "bookmark")
                             .font(.system(size: 25, weight: .light))
                             .imageScale(.medium)
                             .frame(width: 40, height: 40)
-                            .padding(.trailing, 20)
+                        NavigationLink(destination: CartView()){
+                            Image(systemName: "cart")
+                                .foregroundColor(.black)
+                                .font(.system(size: 25, weight: .light))
+                                .imageScale(.medium)
+                                .frame(width: 40, height: 40)
+                                .padding(.trailing, 20)
+                        }
+                    }
+                    //뷰페이저 -> 일단은 이미지로 붙여요
+                    HStack{
+                        Image("home")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
                     }
                 }
-                //뷰페이저 -> 일단은 이미지로 붙여요
-                HStack{
-                    Image("home")
+                if showSplash {
+                    Image("splash1")
                         .resizable()
-                        .aspectRatio(contentMode: .fit)
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                withAnimation {
+                                    showSplash2 = true
+                                    showSplash = false
+                                }
+                            }
+                        }
+                }
+                
+                if showSplash2 {
+                    Image("splash2")
+                        .resizable()
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                withAnimation {
+                                    showSplash2 = false
+                                }
+                            }
+                        }
                 }
             }
         }
+        
+        //        .navigationBarHidden(true)
         .navigationBarTitleDisplayMode(.inline)
         .navigationViewStyle(StackNavigationViewStyle())
     }
