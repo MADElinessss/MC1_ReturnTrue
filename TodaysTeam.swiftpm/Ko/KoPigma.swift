5c3f768ee2e016df5d5039be01960a71fdc07e2e
tmd4669 <tmd4669@naver.com>
2023-03-30 17:41:55 +0900

    koong style 추가

---
+++ TodaysTeam.swiftpm/Ko/KoPigma.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by 이승준 on 2023/03/30.
+//
+
+import SwiftUI
+
+extension Style {
+    static var eren: [Style] = [
+        .style1(image: "Ko_St1", title: "Koong \n \n # Stay hungry, Stay foolish \n # 끌리면 하라"),
+        .style9(image: "Ko_St9", title: "\"Koong\"", subTitle: "사람을, 좋아해요!\n새로운 것을 배우고 싶어요"),
+        .style4(image: ["Ko_St4_1", "Ko_St4_2","Ko_St_3"]),
+        .style7(title: "<본인 기본 정보> \n 이름: 이승준 \n 퍼스널컬러: 푸른색 \n 파트: 도매인 \n 혈액형: B형 \n MBTI: ENFJ \n SwiftUI: 배우고 있습니다... \n \n <남이 쓰는 나의 프로필> \n 매력 포인트: 신나서 말할 때 입모양 \n 잘어울리는 노래는?: 케로로 행진곡(opening) \n 떠오르는 색은? 찐한 오렌지 \n 쿵이 행복해보는 순간은: 키오를 향할 때"),
+        .style2(image: "Ko_St2", title: "사람과 사람이 만나 새로운 아이디어를 만듭니다.")
+    ]
+}
