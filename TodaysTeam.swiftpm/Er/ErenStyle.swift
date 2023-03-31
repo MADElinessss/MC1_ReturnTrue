f00a4af5233d3963a327b4f981aea1b31f3e6f79
Moon <59304977+mun9769@users.noreply.github.com>
2023-03-31 13:10:26 +0900

    ErenStyle 수정

--- TodaysTeam.swiftpm/Er/ErenStyle.swift
+++ TodaysTeam.swiftpm/Er/ErenStyle.swift
@@ -7,13 +7,14 @@
 
 extension Style {
     static var eren: [Style] = [
-        .style11(image: "eren", title: "안녕하세요안녕하세요\n안녕하세요안녕하세요\n안녕하세요안녕하세요\n안녕하세요안녕하세요"),
-        .style10(title: "메인랩에서 수염있는 사람을 본 적이 없다고요?", subTitle: "제가 나중에 길러볼게요 ㅎ"),
-        .style2(image: "eren_songdo", title: "저는 여행을 좋아해요"),
-        .style6(image: "eren_busan"),
-        .style3(image: "eren_jeju"),
-        .style14(image: "eren_asan"),
+        .style11(image: "eren", title: "안녕하세요안녕하세요\n안녕하세요안녕하세요\n안녕하세요안녕하세요\n안녕하세요안녕하세요\n안녕하세요안녕하세요\n"),
         .style4(image: ["eren_flower1", "eren_flower2", "eren_flower3"]),
-        .style5(title: "낯을 조금 가려요", subTitle: "저도 다가가려고 노력할게요\n우리 조금씩 친해져요 :)")
+        .style6(image: "eren_jeju"),
+        .style8(image: "eren_game", title: "저는 게임을 좋아해요", subTitle: "스유로 만든 플래피버드 게임이에요\n개발은 계속 진행중이에요 :)"),
+        .style3(image: "eren_busan"),
+        .style5(title: "프로젝트제작 성공하시려면 저 에렌과 진행하셔야 합니다", subTitle: "1. 무료상담 또는 기초질의서 작성\n2. 작업 가능 여부 및 견적과 기간 안내\n3.팀원과의 커뮤니케이션을 통해 공동의 서비스 기획\n4. 서비스 목표와 연계된 기능 파악\n5. 매시간 팀원들과 피드백 회의 진행\n6. 피드백 반영 및 추가 작업 진행"),
+        .style14(image: "eren_asan"),
+        .style5(title: "MC1에서 많이 배우고 즐겁게 프로젝트 진행했어요", subTitle: "같이 재밌게 프로젝트해요"),
+        .style5(title: "저는 백엔드를 공부하고 있어요", subTitle: "서버가 필요하다면 저를 찾아주세요"),
     ]
 }
